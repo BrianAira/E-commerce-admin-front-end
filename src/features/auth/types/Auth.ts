1d3081884df15bff6d93f9500c4bd2f499d647ed
Brian Aira <136931601+BrianAira@users.noreply.github.com>
2026-04-17 22:19:11 -0300

    feat(auth):definir interfaces de tipos

--- src/features/auth/types/Auth.ts
+++ src/features/auth/types/Auth.ts
@@ -1,19 +1,29 @@
-export interface LoginPayload{
-    email: string,
-    password:string,
-}
-
-export interface RegisterPayload{
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    address: string;
+// Lo que enviamos al backend
+export interface LoginPayload {
+    username: string; // El email del usuario
     password: string;
-    role: string; 
 }
 
-export interface AuthReponse{
-    access_token:string;
-    token_type:string;
+// La respuesta del login (Estándar OAuth2 de FastAPI)
+export interface AuthResponse {
+    access_token: string;
+    token_type: string;
+}
+
+// La respuesta de /users/me
+export interface UserMeResponse {
+    id: number;
+    username: string;
+    email: string;
+    role: 'admin' // Definimos los roles literales
+    // directions: any[]; // Puedes tipar esto más adelante si lo usas
+}
+
+// El estado completo que guardaremos en Redux
+export interface AuthState {
+    user: UserMeResponse | null;
+    token: string | null;
+    isAuthenticated: boolean;
+    isLoading:boolean;
+    error:string|null;
 }
